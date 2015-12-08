2cbfdd62d295bffd15be5a5ea05955e3a0501155
HampusJava15 <hampus.jarkmyr@gmail.com>
2015-12-08 14:27:52 +0100

    hämta fil, ny fil och 'q' för quit

---
+++ texteditor/src/main/java/se/yh/java15/texteditor/TexteditorDemo.java
@@ -0,0 +1,15 @@
+package se.yh.java15.texteditor;
+
+public class TexteditorDemo {
+
+	public static void main(String[] args) {
+		
+		TextEditor textEditor = new TextEditor();
+		
+		textEditor.getPath();
+		System.out.println("");
+		textEditor.showMenu();
+
+	}
+
+}
