2a4d1740c230d27936e466ca6fdcc2c82073c51a
HampusJava15 <hampus.jarkmyr@gmail.com>
2015-12-08 15:03:50 +0100

    h

--- texteditor/src/main/java/se/yh/java15/texteditor/TextEditor.java
+++ texteditor/src/main/java/se/yh/java15/texteditor/TextEditor.java
@@ -69,15 +69,15 @@ public class TextEditor {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Menu");
-		System.out.println("a- add new material in the end of the file");
-		System.out.println("d+number..number- erase column from number..number");
-		System.out.println("g- get file");
-		System.out.println("h- help");
-		System.out.println("i+number- add column after choosen number");
-		System.out.println("l- to print out the whole/parts of the file");
-		System.out.println("m- Show menu again");
-		System.out.println("q- Quit");
-		System.out.println("s- Save");
+		System.out.println("a - add new material in the end of the file");
+		System.out.println("d<number..number> - erase column from number..number");
+		System.out.println("g - get file");
+		System.out.println("h - help");
+		System.out.println("i<number> - add column after choosen number");
+		System.out.println("l - to print out the whole/parts of the file");
+		System.out.println("m - Show menu again");
+		System.out.println("q - Quit");
+		System.out.println("s - Save");
 		System.out.println("=====================================");
 		System.out.println("Your choice: ");
 		String choice = sc.nextLine();
